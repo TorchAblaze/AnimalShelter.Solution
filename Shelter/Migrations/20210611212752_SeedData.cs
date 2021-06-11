fc1311ea252fc7d8d5168d38f3e90a7747b04d2e
TorchAblaze <tiffanygreathead@gmail.com>
2021-06-11 14:29:09 -0700

    Add seed data, think of decent pet names

---
+++ Shelter/Migrations/20210611212752_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "AdoptionStatus", "ApproximateAge", "Biography", "Breed", "FluffinessLevel", "HasShots", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "unadopted", 0, "Likes green apples, long walks on the beach, and eyepatches", "Pitbull Mix", 6, true, "Captian Furbossa", "dog" },
+                    { 2, "pending", 0, "Likes looking out windows, chasing lasers, and adventuring into laundry baskets", "Norwegian Forest Cat", 10, true, "Pawme Amidala", "cat" },
+                    { 3, "unadopted", 0, "Likes sniffing, climbing on everything, and intelligent converstaions", "Brussels Griffon Mix", 7, true, "Wicket", "dog" },
+                    { 4, "unadopted", 0, "Likes hiding things that don't belong to him, barking orders, and naps", "unknown", 4, true, "Admiral Ackbark", "dog" },
+                    { 5, "unadopted", 0, "Likes causing chaos, destroying everything in sight, and belly rubs", "Birman", 6, true, "Catsian", "cat" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
--- Shelter/Models/ShelterContext.cs
+++ Shelter/Models/ShelterContext.cs
@@ -9,5 +9,17 @@ namespace Shelter.Models
     }
 
     public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Captian Furbossa", AdoptionStatus = "unadopted", Biography = "Likes green apples, long walks on the beach, and eyepatches", Type = "dog", Breed = "Pitbull Mix", HasShots = true, FluffinessLevel = 6 },
+          new Animal { AnimalId = 2, Name = "Pawme Amidala", AdoptionStatus = "pending", Biography = "Likes looking out windows, chasing lasers, and adventuring into laundry baskets", Type = "cat", Breed = "Norwegian Forest Cat", HasShots = true, FluffinessLevel = 10 },
+          new Animal { AnimalId = 3, Name = "Wicket", AdoptionStatus = "unadopted", Biography = "Likes sniffing, climbing on everything, and intelligent converstaions", Type = "dog", Breed = "Brussels Griffon Mix", HasShots = true, FluffinessLevel = 7 },
+          new Animal { AnimalId = 4, Name = "Admiral Ackbark", AdoptionStatus = "unadopted", Biography = "Likes hiding things that don't belong to him, barking orders, and naps", Type = "dog", Breed = "unknown", HasShots = true, FluffinessLevel = 4 },
+          new Animal { AnimalId = 5, Name = "Catsian", AdoptionStatus = "unadopted", Biography = "Likes causing chaos, destroying everything in sight, and belly rubs", Type = "cat", Breed = "Birman", HasShots = true, FluffinessLevel = 6 }
+        );
+    }
   }
 }
